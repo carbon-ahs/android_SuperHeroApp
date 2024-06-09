6cb2813dfcb0389e27ad4bce11334da079d59b04
Ahsan Habib <ict.wsd19@waltonplc.com>
2024-06-09 11:21:54 +0600

    material design added

---
+++ app/src/main/java/com/axiagroups/superheroapp/domain/model/Hero.kt
@@ -0,0 +1,11 @@
+package com.axiagroups.superheroapp.domain.model
+
+
+/**
+ * Created by Ahsan Habib on 6/9/2024.
+ */
+data class Hero(
+    val nameRes: Int,
+    val descriptionRes: Int,
+    val imageRes: Int,
+)
